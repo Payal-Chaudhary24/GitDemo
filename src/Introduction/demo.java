e6dabe9437c596c7f0dd824604ac26488bc6a3c4
payal-chaudhary24 <payalpadwekar9@gmail.com>
2025-05-07 09:49:48 -0700

    Architect Asian development

---
+++ src/Introduction/demo.java
@@ -0,0 +1,19 @@
+package Introduction;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+	public void Android()
+	{
+		System.out.println("Androidapps");
+		
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
